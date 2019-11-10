0e38aefd55ced8d573c1d526be4f902948eee2f3
Christoph Schreyer <christophschreyer@web.de>
2019-11-10 09:52:33 +0100

    Update setcron.sh

--- setcron.sh
+++ setcron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo -e "$(env)\n" > /etc/crontab
-echo -e "0 2 * * 2 root python3 /root/praktomat_grading.py\n#" >> /etc/crontab
+echo -e "0 2 * * 1 root python3 /root/praktomat_grading.py\n#" >> /etc/crontab
